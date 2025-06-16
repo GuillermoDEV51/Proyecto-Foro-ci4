e0ddb841d9a4b124a74fb63613fadfdaefa7c057
GuillermoDEV51 <guillermodev2@gmail.com>
2025-06-16 19:08:55 -0400

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,11 +12,15 @@ $routes->get('/', 'Home::index');
 
 /* */
 
-// routas para
+// routas para proyectos
 $routes->get('/proyectos', 'proyectos::index');
 
 
+// routas para ayuda
+$routes->get('/ayuda', 'ayuda::index');
 
+// routas para contacto
+$routes->get('/contacto', 'contacto::index');
 
 
 // routas para el login
