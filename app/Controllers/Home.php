e1819df6fbd95bf2c82a08c43e4a87a4fca40919
Alarconwth <jalxrcon18@gmail.com>
2025-06-27 00:05:47 -0400

    Agregado de documentos destacados

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -35,8 +35,17 @@ class Home extends BaseController
         'carrera' => 'Ingeniería Informática',
         'pdf' => 'AlgoritmosGeneticos.pdf',
         'imagen' => 'https://images.unsplash.com/photo-1617839625591-e5a789593135?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/300x200',
-        ]
-        // Agrega más si lo deseas
+        ],
+         [
+            'id' => 104,
+            'titulo' => 'Redes Inteligentes',
+            'autor' => 'Ana Martínez',
+            'anio' => 2023,
+            'imagen' => 'https://images.unsplash.com/photo-1617839625591-e5a789593135?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/300x200',
+        ],
+
+
+  // Agrega más si lo deseas
     ];
 
     return view('home', ['destacados' => $destacados]);
